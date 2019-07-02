679d355a0e0cc51645c1ca575c6f2035a60a2579
Minh <mvn27@drexel.edu>
2019-07-02 13:45:03 +0700

    uploadThu3

---
+++ test1.py
@@ -0,0 +1,73 @@
+import os
+import numpy as np
+from scipy import misc                     # Import tat ca moi thu can
+import cv2
+import matplotlib.pyplot as plt
+np.random.seed(9)
+
+# Doc file
+path = 'D:/Minh/Python/YALE/unpadded/' # path to the database
+ids = range(1, 16) # 15 persons
+states = ['centerlight', 'glasses', 'happy', 'leftlight',
+          'noglasses', 'normal', 'rightlight','sad',
+          'sleepy', 'surprised', 'wink' ]
+prefix = 'subject'
+surfix = '.pgm'
+
+# data dimension
+h = 116 # hight
+w = 98 # width
+D = h * w
+N = len(states)*15
+K = 100
+
+# collect all data
+X = np.zeros((D, N))
+cnt = 0
+for person_id in range(1, 16):
+    for state in states:
+        fn = path + prefix + str(person_id).zfill(2) + '.' + state + surfix
+        X[:, cnt] = misc.imread(fn).reshape(D)
+        cnt += 1
+
+# Doing PCA, note that each row is a datapoint
+from sklearn.decomposition import PCA
+pca = PCA(n_components=K) # K = 100
+pca.fit(X.T)
+
+# projection matrix
+U = pca.components_.T
+for person_id in range(1, 7):
+    for state in ['centerlight']:
+        fn = path + prefix + str(person_id).zfill(2) + '.' + state + surfix
+        im = misc.imread(fn)
+        plt.axis('off')
+        f1 = plt.imshow(im, interpolation='nearest')
+        f1.axes.get_xaxis().set_visible(True)
+        f1.axes.get_yaxis().set_visible(False)
+        plt.gray()
+        fn = 'ori' + str(person_id).zfill(2) + '.png'
+        plt.savefig(fn, bbox_inches='tight', pad_inches=0)
+        plt.show()
+        
+        # reshape and subtract mean
+        x = im.reshape(D, 1) - pca.mean_.reshape(D, 1)
+        # encode
+        z = U.T.dot(x)
+        #decode
+        x_tilde = U.dot(z) + pca.mean_.reshape(D, 1)
+        
+
+        # reshape to orginal dim
+        im_tilde = x_tilde.reshape(116, 98)
+        loss = np.sqrt(np.sum((im - im_tilde)**2))
+        loss /= im.size
+        print(loss)
+        plt.axis('on')
+        f1 = plt.imshow(im_tilde, interpolation='nearest')
+        f1.axes.get_xaxis().set_visible(False)
+        f1.axes.get_yaxis().set_visible(False)
+        plt.gray()
+        fn = 'res' + str(person_id).zfill(2) + '.png'
+        plt.savefig(fn, bbox_inches='tight', pad_inches=0)
+        plt.show()
